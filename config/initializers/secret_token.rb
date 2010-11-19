42b93061c4bcdd560b8c537d758671121e2987cd
Yin Yin Wu <yinyinwu5@yahoo.com>
2010-11-18 21:18:51 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StanfordMem::Application.config.secret_token = 'e8654772e51d010d4cdfc72342da5e7c8641e1ab589e25db07672de1601b357dbdd5e91f4b35f7ec9f5829e36fd158990e0e6c73132e0a3364edadd59120c520'
